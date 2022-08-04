dcd78e1a25641efe26f6c79bebda037cffe40144
shaik apsar <apsarsk823@gmail.com>
2022-08-04 19:53:49 +0530

    learner-react

---
+++ src/HeaderPage.js
@@ -0,0 +1,8 @@
+
+import React from "react"
+
+export default function HeaderPage(){
+    return <div>
+        <h2>nextjs app</h2>
+    </div>
+}
---
+++ src/Homepage.js
@@ -0,0 +1,12 @@
+
+import react from 'react'
+import HeaderPage from './HeaderPage'
+
+export default function HomePage(){
+    return <div>
+        <h1>apsar learning react</h1>
+        
+      <HeaderPage></HeaderPage>
+    
+    </div>
+}
